b2c908e2601aecba0a12e424c42968a204dab971
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-20 22:29:38 +0100

    solution type

--- solutions1 .rb
+++ solutions1 .rb
@@ -1,3 +1,2 @@
-# Create your own LinkedList  class for storing integers which should include two methods:  
-# add(number) - Add a new Node to the end of the list with a value of number. Nothing needs to be returned. 
-# get(index) - Return the value of the Node at position `index` in the List.   
+#Solution 1.1
+
