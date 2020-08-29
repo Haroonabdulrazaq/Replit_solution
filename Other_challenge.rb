909d5af2a6358477890cc2aad9031aba35cc19e0
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-29 21:25:02 +0100

    Add Algorithm for the solution

---
+++ Other_challenge.rb
@@ -0,0 +1,8 @@
+#Algorithm
+#init_value is 3
+#start_time is 1
+#count down the init_time
+#Increase the start_time
+#If init_time is 1 double the init_time
+#Keep doing until start_time == t
+#return init_value
