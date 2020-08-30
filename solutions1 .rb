b9fd628474a263fab9b2826a4b87d4adaf6bc968
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-30 21:40:02 +0100

    Add solution for test cases

--- solutions1 .rb
+++ solutions1 .rb
@@ -260,3 +260,24 @@ class Queue
     end
   end
 end
+
+queue = Queue.new
+
+queue.add(3)
+queue.add(5)
+puts queue.remove
+# => 3
+
+queue.add(2)
+queue.add(7)
+puts queue.remove
+# => 5
+
+puts queue.remove
+# => 2
+
+puts queue.remove
+# => 7
+
+puts queue.remove
+# => -1
