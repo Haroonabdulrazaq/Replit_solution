a709d5b4b1f35bcb8cb2d4cb0ce180b6203e1c57
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-11-16 11:21:20 +0100

    Add solution for balanced brackets

--- solutions1 .rb
+++ solutions1 .rb
@@ -281,3 +281,113 @@ puts queue.remove
 
 puts queue.remove
 # => -1
+
+class Node
+  attr_accessor :value, :next_node
+  def initialize(value)
+    @value = value
+  end
+end
+
+class Stack 
+ def initialize
+  @head = nil 
+  @tail = nil
+  @min = nil
+ end
+
+  def push(number)
+    new_node = Node.new(number)
+    if @head.nil?
+      @head = new_node
+      @tail = @head
+    else
+      @tail.next_node = new_node
+      @tail = @tail.next_node
+    end
+  end
+
+  def pop 
+    if @head.nil? 
+      "Error, Stack is Empty"
+    else
+      current = @head
+      pop = @tail
+      while current.next_node != @tail
+        current = current.next_node
+      end
+       @tail = current
+       @tail.next_node = nil
+       pop.value 
+    end
+  end
+
+
+  def min
+    current = @head
+    @min = @head # Setting back @min to head
+    until current.next_node.nil?
+      current = current.next_node
+      @min = current if @min.value > current.value
+    end
+      @min.value
+  end
+end
+
+stack = Stack.new
+
+stack.push(3)
+stack.push(5)
+puts stack.min
+# => 3
+
+stack.pop
+stack.push(7)
+puts stack.min
+# => 3
+
+stack.push(2)
+puts stack.min
+# => 2
+
+stack.pop
+puts stack.min
+
+def balanced_brackets?(string)
+  str = string.gsub(/\w/, "")
+  strArr = str.split('')
+
+ return false if strArr.length.odd?
+
+ return false if strArr[0] == ')' || strArr[0] == '}' || strArr[0] == ']'
+
+ strArr.length.times do |i|
+  case strArr[i]
+    when '('
+      if strArr[i + 1] == '}' || strArr[i + 1] == ']'
+        return false
+      end
+    when '{'
+      if strArr[i + 1] == ')' || strArr[i + 1] == ']'
+        return false
+      end
+    when '['
+      if strArr[i + 1] == ')' || strArr[i + 1] == '}'
+        return false
+      end 
+    else
+      return true
+    end
+  end
+end
+
+puts balanced_brackets?('(hello)[world]')
+# => true
+
+puts balanced_brackets?('([)]')
+# => false
+puts balanced_brackets?('(')
+# => false
+
+puts balanced_brackets?('[({}{}{})([])]')
+# => true
