8445ee4cb2e448f1770e898d1e13478ca692ec00
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-11-16 11:20:16 +0100

    Add solution for balanced bracket

--- new.rb
+++ new.rb
@@ -1,70 +1,38 @@
-class Node
-  attr_accessor :value, :next_node
-  def initialize(value)
-    @value = value
-  end
-end
+def balanced_brackets?(string)
+  str = string.gsub(/\w/, "")
+  strArr = str.split('')
 
-class Stack 
- def initialize
-  @head = nil 
-  @tail = nil
-  @min = nil
- end
+ return false if strArr.length.odd?
 
-  def push(number)
-    new_node = Node.new(number)
-    if @head.nil?
-      @head = new_node
-      @tail = @head
-    else
-      @tail.next_node = new_node
-      @tail = @tail.next_node
-    end
-  end
+ return false if strArr[0] == ')' || strArr[0] == '}' || strArr[0] == ']'
 
-  def pop 
-    if @head.nil? 
-      "Error, Stack is Empty"
-    else
-      current = @head
-      pop = @tail
-      while current.next_node != @tail
-        current = current.next_node
+ strArr.length.times do |i|
+  case strArr[i]
+    when '('
+      if strArr[i + 1] == '}' || strArr[i + 1] == ']'
+        return false
       end
-       @tail = current
-       @tail.next_node = nil
-       pop.value 
+    when '{'
+      if strArr[i + 1] == ')' || strArr[i + 1] == ']'
+        return false
+      end
+    when '['
+      if strArr[i + 1] == ')' || strArr[i + 1] == '}'
+        return false
+      end 
+    else
+      return true
     end
-  end
-
-
-  def min
-    current = @head
-    @min = @head
-    until current.next_node.nil?
-      current = current.next_node
-      @min = current if @min.value > current.value
-    end
-      @min.value
   end
 end
 
-stack = Stack.new
+puts balanced_brackets?('(hello)[world]')
+# => true
 
-stack.push(3)
-stack.push(5)
-puts stack.min
-# => 3
+puts balanced_brackets?('([)]')
+# => false
+puts balanced_brackets?('(')
+# => false
 
-stack.pop
-stack.push(7)
-puts stack.min
-# => 3
-
-stack.push(2)
-puts stack.min
-# => 2
-
-stack.pop
-puts stack.min
+puts balanced_brackets?('[({}{}{})([])]')
+# => true
