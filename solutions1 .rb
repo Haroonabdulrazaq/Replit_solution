16eb20c47d8c612fd3537bb55d022a52899dcd9f
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-07 11:24:33 +0100

    Add solution to tower of Hanoi

--- solutions1 .rb
+++ solutions1 .rb
@@ -507,3 +507,22 @@ def transpose(string)
   # else if midpoint*midpoint is less than number
     # then Shift Start to midpoint[index + 1]
     # then get midpoint again
+
+    # Tower of Hanoi basic for 2 disk problem
+    def move(starting, goal)
+      intermediate = ([1,2,3] - [ starting, goal]).first
+      result = move_text(starting, intermediate)+" "+
+      result = move_text(starting, goal) +" "+
+      result = move_text(intermediate, goal)
+      return result
+    end
+    
+    def move_text(from, to)
+      "#{from}->#{to}"
+    end
+    
+    puts move(1, 3)
+    # => 1->2 1->3 2->3
+    
+    puts move(2, 3)
+    # => 2->1 2->3 1->3
