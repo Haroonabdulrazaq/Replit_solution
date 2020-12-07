2a4ff017ce1e916e2eff439768fc34c62cd6fec7
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-07 11:25:54 +0100

    Add models solution for tower of Hanoi

--- solutions1 .rb
+++ solutions1 .rb
@@ -526,3 +526,17 @@ def transpose(string)
     
     puts move(2, 3)
     # => 2->1 2->3 1->3
+
+   # Add Model's Solution for Tower of Hanoi
+    def move(starting, goal)
+      # your code here
+      intermediate = (1..3).find { |n| n != starting && n != goal }
+      "#{starting}->#{intermediate} #{starting}->#{goal} #{intermediate}->#{goal}"
+    end
+    
+    puts move(1, 3)
+    # => 1->2 1->3 2->3
+    
+    puts move(2, 3)
+    # => 2->1 2->3 1->3
+    
