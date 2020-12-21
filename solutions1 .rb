14f8310573c73e6e696c3357f838d31094a54ce1
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-21 11:01:28 +0100

    Add solution to backtracking algorithm

--- solutions1 .rb
+++ solutions1 .rb
@@ -572,5 +572,21 @@ def transpose(string)
     # 2->1 intermediate-> start
     # 2->3 intermediate-> goal
     # 1->3 start-> goal
+
+
+    # Backtracking Algorithm
+    def exact_sum?(k, coins)
+      return true if k == 0
+      return false if k < 0 || coins.empty?
+    
+    
+      exact_sum?(k - coins[0], coins[1,coins.length]) || exact_sum?(k, coins[1, coins.length])
+    end
+    
+    puts exact_sum?(12, [1, 2, 3, 4, 5])
+    # => true
+    
+    puts exact_sum?(11, [1, 5, 9, 13])
+    # => false
     
     
