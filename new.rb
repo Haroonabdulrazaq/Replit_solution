25bf3afd9431492705902f15d55b2ee51c5100a2
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-21 10:58:34 +0100

    Solve backtracking algorithm

--- new.rb
+++ new.rb
@@ -6,7 +6,7 @@ def exact_sum?(k, coins)
   exact_sum?(k - coins[0], coins[1,coins.length]) || exact_sum?(k, coins[1, coins.length])
 end
 
-# puts exact_sum?(12, [1, 2, 3, 4, 5])
+puts exact_sum?(12, [1, 2, 3, 4, 5])
 # => true
 
 puts exact_sum?(11, [1, 5, 9, 13])
