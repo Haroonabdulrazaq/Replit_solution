3e322b9dd894b98fada9f3cbcfec2f619fe1defc
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-28 08:13:25 +0100

    Add model soution for How tall is the tree

--- new.rb
+++ new.rb
@@ -1,5 +1,69 @@
+class Node
+  attr_reader :data
+  attr_accessor :left, :right
+
+  def initialize(data)
+    @data = data
+  end
+end
+
+class BST
+  # node = Node.new(node)
+  # def insert(node)
+  #   current = @root
+    
+  #   result = current
+  #   until current.nil?  
+  #     if node.data <= current.data
+  #       current = current.left
+  #       result += current
+  #     elsif node.data > current.data
+  #       current = current.right
+  #       result += current
+  #     end
+  #   end
+  # end
+
+  def insert(new_node, head = @root)
+    # base case: catch if the tree is empty
+    if @root.nil?
+      @root = new_node
+      return
+    end
+    # it helps to break the recursion when you find the empty spot
+    if head.nil?
+      return new_node
+    end
+    # if the new node is less than the head, go to the left descendant
+    if new_node.data < head.data
+      head.left = insert(new_node, head.left)
+    # if the new node is greater than the head, go to the right descendant
+    elsif new_node.data > head.data
+      head.right = insert(new_node, head.right)
+    end
+    # return the calculated head
+    return head
+  end
 
 
+  def pre_order(node = @root)
+    if node == nil
+      return ''
+    elsif node.data != nil 
+
+    end
+    result = "#{node.data}, "
+    result += pre_order(node.left)
+    result += pre_order(node.right)
+  end
+end
 
 
+def binary_search_tree(array)
+  tree = BST.new
+  array.each { |e| tree.insert(Node.new(e)) }
+  tree.pre_order
+end
 
+puts binary_search_tree([8, 3, 10, 1, 6, 14, 4, 7, 13])
+# => "8 3 1 6 4 7 10 14 13"
--- solutions1 .rb
+++ solutions1 .rb
@@ -680,3 +680,40 @@ def binary_tree_height(array_tree)
 end
 
 puts binary_tree_height([2, 4, 8, 0, 9, 6, 9])
+
+# How tall is the Tree==> Solution by Model
+
+class Node
+  attr_reader :data
+  attr_accessor :left, :right
+
+  def initialize(data)
+    @data = data
+  end
+end
+
+def array_to_tree(array, i)
+  return nil if i >= array.length || array[i] == 0
+
+  node = Node.new(array[i])
+  node.left = array_to_tree(array, 2*i+1)
+  node.right = array_to_tree(array, 2*i+2)
+
+  node
+end
+
+def binary_tree_height(array_tree)
+  # write your code here
+  tree = array_to_tree(array_tree, 0)
+  
+  recursive_tree_height(tree)
+end
+
+def recursive_tree_height(tree)
+  return 0 if tree.nil?
+  return 1 if tree.left == nil && tree.right == nil
+  
+  [recursive_tree_height(tree.left), recursive_tree_height(tree.right)].max + 1
+end
+
+puts binary_tree_height([2, 7, 5, 2, 6, 0, 9])
