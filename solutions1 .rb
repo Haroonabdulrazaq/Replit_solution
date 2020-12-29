7ef6ea4d39accbcf01330981b84077e3e9b169f8
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-29 11:07:54 +0100

    Add solution to binary search

--- solutions1 .rb
+++ solutions1 .rb
@@ -717,3 +717,55 @@ def recursive_tree_height(tree)
 end
 
 puts binary_tree_height([2, 7, 5, 2, 6, 0, 9])
+
+
+
+# Binary Serach Solution
+class Node
+  attr_reader :data
+  attr_accessor :left, :right
+
+  def initialize(data)
+    @data = data
+  end
+end
+
+class BST
+  def insert(new_node, head = @root)
+    if @root.nil?
+      @root = new_node
+    end 
+    if head.nil?
+      return new_node
+    end 
+    
+    if new_node.data < head.data
+      head.left = insert(new_node, head.left)
+    elsif new_node.data > head.data
+      head.right = insert(new_node, head.right)
+    end
+    return head
+  end
+
+  def pre_order(node = @root)
+    if node.nil? 
+      return ''
+    elsif node.data != nil 
+    
+    end
+    
+    result = "#{node.data} "
+    result += pre_order(node.left)
+    result += pre_order(node.right)
+  end
+end
+
+
+def binary_search_tree(array)
+  tree = BST.new
+  array.each { |e| tree.insert(Node.new(e)) }
+  tree.pre_order
+end
+
+puts binary_search_tree([8, 3, 10, 1, 6, 14, 4, 7, 13])
+# => "8 3 1 6 4 7 10 14 13"
