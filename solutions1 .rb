5e20150570fe9b9ff85f4a2ec7aea3890a98a428
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-12-31 10:55:37 +0100

    Add Is it a search tree to solution

--- solutions1 .rb
+++ solutions1 .rb
@@ -769,3 +769,41 @@ end
 
 puts binary_search_tree([8, 3, 10, 1, 6, 14, 4, 7, 13])
 # => "8 3 1 6 4 7 10 14 13"
+
+# Is it a search Tree
+
+class Node
+	attr_reader :data
+	attr_accessor :left, :right
+	
+	def initialize data
+		@data = data
+	end
+end
+
+def array_to_tree(array, i = 0)
+	return nil if i >= array.length || array[i] == 0
+	
+	node = Node.new(array[i])
+	node.left = array_to_tree(array, 2*i+1)
+	node.right = array_to_tree(array, 2*i+2)
+	
+	node
+end
+
+  def search_tree?(array)
+	  root = array_to_tree(array)
+   
+    search_helper(root) 
+  end
+
+  def search_helper (node, min = nil, max = nil)
+  return true if node.nil?
+  return false if min && node.data < min
+  return false if max && node.data > max
+  search_helper(node.left, min, node.data) && search_helper(node.right, node.data, max)
+end
+
+
+ puts search_tree?([20, 10, 27, 12, 14, 23, 30])
+# => false
